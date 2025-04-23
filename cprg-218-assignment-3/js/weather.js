f4c7c8594d5f42d67e5f8f147ef680fdd56c3e87
angelobaloso <160713947+angelobaloso@users.noreply.github.com>
2025-04-22 22:20:42 -0600

    Will stay with multiple JS.
    Updated weather.js

--- cprg-218-assignment-3/js/weather.js
+++ cprg-218-assignment-3/js/weather.js
@@ -1,11 +1,9 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // const apiKey = '7b76b4e7fff204285ed11d45b50e2d65';
+document.addEventListener('DOMContentLoaded', function () {
+    const apiKey = '7b76b4e7fff204285ed11d45b50e2d65';
     const lat = 21.1619;
     const lon = -86.8515;
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
-    
     const weatherDisplay = document.getElementById('weather-display');
-    
     fetch(apiUrl)
         .then(response => {
             if (!response.ok) {
@@ -16,30 +14,31 @@ document.addEventListener('DOMContentLoaded', function() {
         .then(data => {
             const temperature = Math.round(data.main.temp);
             const weatherDescription = data.weather[0].description;
-            const windSpeed = Math.round(data.wind.speed * 3.6); // Convert m/s to km/h
-            
-            // weather icons depending on conditions
+            const windSpeed = Math.round(data.wind.speed * 3.6);
+
             let weatherIcon;
             const weatherCode = data.weather[0].id;
-            
+
             if (weatherCode >= 200 && weatherCode < 300) {
                 weatherIcon = '⛈️';
-            } else if (weatherCode >= 300 && weatherCode < 500) {
-                weatherIcon = '🌧️';
-            } else if (weatherCode >= 500 && weatherCode < 600) {
+            } else if (weatherCode >= 300 && weatherCode < 400) {
+                weatherIcon = '🌦️';
+            } else if (weatherCode >= 400 && weatherCode < 600) {
                 weatherIcon = '🌧️';
             } else if (weatherCode >= 600 && weatherCode < 700) {
-                weatherIcon = '❄️';
+                weatherIcon = '🌡️';
             } else if (weatherCode >= 700 && weatherCode < 800) {
                 weatherIcon = '🌫️';
             } else if (weatherCode === 800) {
                 weatherIcon = '☀️';
-            } else if (weatherCode > 800) {
+            } else if (weatherCode > 800 && weatherCode <= 802) {
+                weatherIcon = '🌤️';
+            } else if (weatherCode > 802) {
                 weatherIcon = '☁️';
             } else {
-                weatherIcon = '🌡️';
+                weatherIcon = '🏝️';
             }
-            
+
             weatherDisplay.innerHTML = `
                 <span>Current conditions: ${temperature}°C ${weatherIcon} Wind ${windSpeed} kph</span>
             `;
